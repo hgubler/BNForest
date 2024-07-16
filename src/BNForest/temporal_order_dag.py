fa53c70a4d44cf1e77757110b809a465ad1f5a4d
hgubler <hannes.gubler@epfl.ch>
2024-07-16 14:03:07 +0200

    add documentation

--- src/BNForest/temporal_order_dag.py
+++ src/BNForest/temporal_order_dag.py
@@ -1,6 +1,13 @@
 import networkx as nx
 
 def temporal_order_dag(ordering):
+    """
+    Function for specifying the causal DAG based on a temporal ordering.
+
+    Parameters:
+    ordering: list 
+        list of lists containg the variable names in the order of the temporal ordering
+    """
     dag = nx.DiGraph()
     for i in range(len(ordering)):
         for j in range(i+1, len(ordering)):
