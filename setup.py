5a8fa8c18f4a43a037b91875e76023b0e977d39e
hgubler <hannes.gubler@epfl.ch>
2024-07-16 10:47:44 +0200

    update setups

--- setup.py
+++ setup.py
@@ -1,30 +1,37 @@
 from setuptools import setup, find_packages
+import subprocess
+import sys
 
-setup(
-    name='BNForest',
-    version='0.1.0',
-    author='Hannes Gubler',
-    author_email='hannes.gubler@epfl.ch',
-    description='A package for generating synthetic data using a Bayesian Network with Random Forests for conditional distributions',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/hgubler/BNForest',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    install_requires=[
-        'numpy',
-        'pandas',
-        'scikit-learn',
-        'networkx',
-        'numba',
-        'openturns',
-        'qosa @ git+https://gitlab.com/qosa_index/qosa.git'
-    ],
-    setup_requires=['numba', 'numpy', 'pandas', 'scikit-learn', 'networkx', 'openturns'],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',
-)
+def install_build_dependencies():
+    """Install build dependencies from requirements-build.txt."""
+    with open('requirements-build.txt') as f:
+        requirements = f.read().splitlines()
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install'] + requirements)
+
+def main():
+    """Run the setup script after installing build dependencies."""
+    install_build_dependencies()
+    setup(
+        name='BNForest',
+        version='0.1.0',
+        author='Your Name',
+        author_email='your.email@example.com',
+        description='A package for Bayesian Network Forest Sampling',
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
+        url='https://github.com/yourusername/BNForest',
+        packages=find_packages(where='src'),
+        package_dir={'': 'src'},
+        install_requires=[
+            'qosa @ git+https://gitlab.com/qosa_index/qosa.git'
+        ],
+        classifiers=[
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+        ],
+        python_requires='>=3.6',
+    )
+
+if __name__ == "__main__":
+    main()
